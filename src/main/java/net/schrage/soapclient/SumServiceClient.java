f05b23c83010f4e32e109012705c718a20cf8d5b
schragman <schragman@gmail.com>
2023-04-11 06:19:16 +0200

    Verschlüsselung klappt

--- src/main/java/net/schrage/soapclient/SumServiceClient.java
+++ src/main/java/net/schrage/soapclient/SumServiceClient.java
@@ -10,6 +10,7 @@ import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 import org.apache.wss4j.dom.WSConstants;
 import org.apache.wss4j.dom.handler.WSHandlerConstants;
+import org.springframework.context.annotation.Bean;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,13 +28,47 @@ public class SumServiceClient {
       Client client = ClientProxy.getClient(sumWs);
       Endpoint endpoint = client.getEndpoint();
       Map<String, Object> props =new HashMap<>();
-      props.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
-      props.put(WSHandlerConstants.USER, "Michael");
+      props.put(WSHandlerConstants.ACTION, "Signature");
+      props.put(WSHandlerConstants.USER, "myclientkey");
       props.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
       props.put(WSHandlerConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+      /*End of WSS4J Security*/
+
+      /*Adding Encryption*/
+      //props.put(WSHandlerConstants.ENCRYPTION_USER, "myclientkey");
+      props.put(WSHandlerConstants.SIG_PROP_FILE, "etc/clientKeystore.properties");
+      /*End Encryption*/
+
+      /*WSS4JOutInterceptor wss4jOutInterceptor = new WSS4JOutInterceptor();
+      wss4jOutInterceptor.setProperty("action", "Encrypt");
+      wss4jOutInterceptor.setProperty("user", "yourUsername");
+      wss4jOutInterceptor.setProperty("encryptionUser", "yourUsername");
+      wss4jOutInterceptor.setProperty("passwordCallbackClass", "com.yourpackage.PasswordCallback");
+      wss4jOutInterceptor.setProperty("encryptionPropFile", "encryption.properties");
+      wss4jOutInterceptor.setProperty("encryptionKeyIdentifier", "IssuerSerial");
+      wss4jOutInterceptor.setProperty("keyIdentifierType", "IssuerSerial");
+      wss4jOutInterceptor.setProperty("secureParts",
+          "{Element}{http://schemas.xmlsoap.org/soap/envelope/}
+*/
+
+     /* @Bean
+      public WSS4JOutInterceptor wss4jOutInterceptor() {
+        Map<String, Object> outProps = new HashMap<>();
+        outProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.ENCRYPT);
+        outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, ClientKeystorePasswordCallback.class.getName());
+        outProps.put(WSHandlerConstants.USER, "client");
+        outProps.put(WSHandlerConstants.ENCRYPTION_USER, "server");
+        outProps.put(WSHandlerConstants.ENCRYPTION_PARTS, "{Content}{http://schemas.xmlsoap.org/soap/envelope/}Body");
+        outProps.put(WSHandlerConstants.ENC_PROP_FILE, "clientKeystore.properties");
+        outProps.put(WSHandlerConstants.SIG_PROP_FILE, "clientKeystore.properties");
+        outProps.put(WSHandlerConstants.USE_REQ_SIG_CERT, "true");
+        outProps.put(WSHandlerConstants.SIG_KEY_ID, "DirectReference");
+        return new WSS4JOutInterceptor(outProps);
+      }*/
+
+
       WSS4JOutInterceptor wssout = new WSS4JOutInterceptor(props);
       endpoint.getOutInterceptors().add(wssout);
-      /*End of WSS4J Security*/
 
       SumRequest sumRequest = new SumRequest();
       sumRequest.setNum1(5);
--- src/main/java/net/schrage/soapclient/UTPasswordCallback.java
+++ src/main/java/net/schrage/soapclient/UTPasswordCallback.java
@@ -7,13 +7,29 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UTPasswordCallback implements CallbackHandler {
+
+  Map<String, String> serverStoredCredentials = new HashMap<>();
+
+  public UTPasswordCallback() {
+    serverStoredCredentials.put("Michael", "geheim");
+    serverStoredCredentials.put("myclientkey", "ckpass");
+  }
   @Override
   public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-    Arrays.stream(callbacks)
+    /*Arrays.stream(callbacks)
         .map(callback -> (WSPasswordCallback) callback)
-        .filter(allServerCredentials -> allServerCredentials.getIdentifier().equals("Michael"))
-        .forEach(serverCredential -> serverCredential.setPassword("geheim"));
+        .filter(transmittedCredential -> serverStoredCredentials.containsKey(transmittedCredential.getIdentifier()))
+        .forEach(transmittedCredential -> transmittedCredential.setPassword(serverStoredCredentials.get(transmittedCredential.getIdentifier())));
+  }*/
+
+    WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+    if (pc.getIdentifier().equals("myclientkey")) {
+      pc.setPassword("cspass");
+    }
   }
+
 }
